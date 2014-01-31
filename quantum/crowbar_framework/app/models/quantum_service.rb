e54ab3aa263f8656b80091923bf6fb73e1c12408
Jez Allen <allen_jez@hotmail.com>
2014-01-31 06:59:43 +0000

    Enforce dependencies

--- quantum/crowbar_framework/app/models/quantum_service.rb
+++ quantum/crowbar_framework/app/models/quantum_service.rb
@@ -25,72 +25,92 @@ class QuantumService < ServiceObject
     false
   end
 
-# commented out for haproxy and percona - sak
-=begin
   def proposal_dependencies(role)
     answer = []
-    if role.default_attributes["quantum"]["use_gitrepo"]
-      answer << { "barclamp" => "git", "inst" => role.default_attributes["quantum"]["git_instance"] }
-    end
-    answer << { "barclamp" => "database", "inst" => role.default_attributes["quantum"]["database_instance"] }
-    answer << { "barclamp" => "rabbitmq", "inst" => role.default_attributes["quantum"]["rabbitmq_instance"] }
-    answer << { "barclamp" => "keystone", "inst" => role.default_attributes["quantum"]["keystone_instance"] }
+    answer << { "barclamp" => "haproxy", "inst" => role.default_attributes[@bc_name]["haproxy_instance"] }
+    answer << { "barclamp" => "percona", "inst" => role.default_attributes[@bc_name]["percona_instance"] }
+    answer << { "barclamp" => "keystone", "inst" => role.default_attributes[@bc_name]["keystone_instance"] }
+    answer << { "barclamp" => "rabbitmq", "inst" => role.default_attributes[@bc_name]["rabbitmq_instance"] }
     answer
   end
-=end
-# end of change
-
+  
   def create_proposal
+    @logger.debug("Quantum create_proposal: entering")
     base = super
 
-    nodes = NodeObject.all
-    nodes.delete_if { |n| n.nil? or n.admin? }
-
-    base["attributes"][@bc_name]["git_instance"] = ""
+    # HAProxy dependency
+    base["attributes"][@bc_name]["haproxy_instance"] = ""
     begin
-      gitService = GitService.new(@logger)
-      gits = gitService.list_active[1]
-      if gits.empty?
+      haproxyService = HaproxyService.new(@logger)
+      haproxys = haproxyService.list_active[1]
+      if haproxys.empty?
         # No actives, look for proposals
-        gits = gitService.proposals[1]
+        haproxys = haproxyService.proposals[1]
       end
-      unless gits.empty?
-        base["attributes"]["quantum"]["git_instance"] = gits[0]
-      end
+      base["attributes"][@bc_name]["haproxy_instance"] = haproxys[0] unless haproxys.empty?
     rescue
-      @logger.info("#{@bc_name} create_proposal: no git found")
+      @logger.info("Quantum create_proposal: no haproxy found")
+    end
+    if base["attributes"][@bc_name]["haproxy_instance"] == ""
+      raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "haproxy"))
     end
 
-# commented out for percona - sak
-=begin
-    base["attributes"]["quantum"]["database_instance"] = ""
+    # Percona dependency
+    base["attributes"][@bc_name]["percona_instance"] = ""
     begin
-      databaseService = DatabaseService.new(@logger)
-      # Look for active roles
-      dbs = databaseService.list_active[1] 
-      if dbs.empty? 
+      perconaService = PerconaService.new(@logger)
+      perconas = perconaService.list_active[1]
+      if perconas.empty?
         # No actives, look for proposals
-        dbs = databaseService.proposals[1]
+        perconas = perconaService.proposals[1]
       end
-      if dbs.empty?
-        @logger.info("Quantum create_proposal: no database proposal found") 
-      else 
-        base["attributes"]["quantum"]["database_instance"] = dbs[0] 
-        @logger.info("Quantum create_proposal: using database proposal: '#{dbs[0]}'")
-      end
+      base["attributes"][@bc_name]["percona_instance"] = perconas[0] unless perconas.empty?
     rescue
-      @logger.info("Quantum create_proposal: no database proposal found") 
+      @logger.info("Quantum create_proposal: no percona found")
+    end
+    if base["attributes"][@bc_name]["percona_instance"] == ""
+      raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "percona"))
     end
 
-    if base["attributes"]["quantum"]["database_instance"] == ""
-      raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "database")) 
+    # Keystone dependency
+    base["attributes"][@bc_name]["keystone_instance"] = ""
+    begin
+      keystoneService = KeystoneService.new(@logger)
+      keystones = keystoneService.list_active[1]
+      if keystones.empty?
+        # No actives, look for proposals
+        keystones = keystoneService.proposals[1]
+      end
+      base["attributes"][@bc_name]["keystone_instance"] = keystones[0] unless keystones.empty?
+    rescue
+      @logger.info("Quantum create_proposal: no keystone found")
     end
-=end
-# end of change
+    if base["attributes"][@bc_name]["keystone_instance"] == ""
+      raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "keystone"))
+    end
 
-    base["deployment"]["quantum"]["elements"] = {
-        "quantum-server" => [ nodes.first[:fqdn] ]
-    } unless nodes.nil? or nodes.length ==0
+    # RabbitMQ dependency
+    base["attributes"][@bc_name]["rabbitmq_instance"] = ""
+    begin
+      rabbitmqService = RabbitmqService.new(@logger)
+      rabbitmqs = rabbitmqService.list_active[1]
+      if rabbitmqs.empty?
+        # No actives, look for proposals
+        rabbitmqs = rabbitmqService.proposals[1]
+      end
+      base["attributes"][@bc_name]["rabbitmq_instance"] = rabbitmqs[0] unless rabbitmqs.empty?
+    rescue
+      @logger.info("Quantum create_proposal: no rabbitmq found")
+    end
+    if base["attributes"][@bc_name]["rabbitmq_instance"] == ""
+      raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "rabbitmq"))
+    end
+
+#    nodes = NodeObject.all
+#    nodes.delete_if { |n| n.nil? or n.admin? }
+#    base["deployment"]["quantum"]["elements"] = {
+#        "quantum-server" => [ nodes.first[:fqdn] ]
+#    } unless nodes.nil? or nodes.length ==0
 
     base["attributes"]["quantum"]["service_password"] = '%012d' % rand(1e12)
     # Set a common password for quantum database user 
@@ -100,27 +120,8 @@ class QuantumService < ServiceObject
 
     # Set a random shared secret for metadata settings
     base["attributes"]["quantum"]["quantum_metadata_proxy_shared_secret"] = random_password
-    insts = ["Keystone", "Rabbitmq"]
-
-    insts.each do |inst|
-      base["attributes"][@bc_name]["#{inst.downcase}_instance"] = ""
-      begin
-        instService = eval "#{inst}Service.new(@logger)"
-        instes = instService.list_active[1]
-        if instes.empty?
-          # No actives, look for proposals
-          instes = instService.proposals[1]
-        end
-        base["attributes"][@bc_name]["#{inst.downcase}_instance"] = instes[0] unless instes.empty?
-      rescue
-        @logger.info("#{@bc_name} create_proposal: no #{inst.downcase} found")
-      end
-
-      if base["attributes"][@bc_name]["#{inst.downcase}_instance"] == ""
-        raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "#{inst.downcase}"))
-      end
-    end
-
+ 
+    @logger.debug("Quantum create_proposal: exiting")
     base
   end
 
